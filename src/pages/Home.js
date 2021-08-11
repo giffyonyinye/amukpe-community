0b0c0d88e60d9db6fd7f862a687b068cff184b1f
Giffy Onyinye <74349914+giffyonyinye@users.noreply.github.com>
2021-08-11 04:35:09 +0100

    made changes in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,17 +15,17 @@ const Home = () => {
 						<br />
 						<p className="subheader_text">
 							Creating Awareness and Opportunities in Amukpe Community.
-							Oneness is strength, love and progress - New Update
+							Oneness is strength, love and progress.
 						</p>
 
 						<br />
 						<br />
 
 						<div className="header_button">
-							<Link to="/portal">	
+							<Link to="/login">	
 								Portal
 							</Link>
-							<Link to="/dashboard">	
+							<Link to="/registration">	
 								Latest Updates
 							</Link>
 						</div>
@@ -52,33 +52,15 @@ const Home = () => {
 						<h2 className="header_text">Achiements & Development</h2>
 						<br />
 						<p className="subheader_text">
-							Development of community billboard and organizing of empowerment 
-							events.
-							Creating job opportunities to indigenes and non-indegenes in the 
-							community.
-							lteh i have the love of the people to enable one understand the
-							 staet of
-							the indigenes and non indegenes import communitySvg 
-							from "../Images/Community.svg"; of the community. building
-							 oursleves
-							 and 
-							understand that the community needs progress and advancment
-							 of the
-							 current 
-							development jygfj hgyuthje yufasydghiuew tyfyssgfuiewyj
-							 udgfytfeuf 
-							ueueyuiru 
-							bytsyjhff yfhteruybs uwdtywjhydu gwyteyw jhghgdhgghhfdgsfdjjcv
-							 yyrrye
-							yfdyf y 
-							dgywqtfiv y ftyewty ud cshgc ctyduwtryu
+							Development of youth empowerment schemes, educating youths on self development and learning skills.  
+							We have succeded in creating job opportunities to indigenes and non-indegenes of Okpe community at large.
 						</p>
 
 						<br />
 						<br />
 
 						<div className="header_button">
-							<Link to="/portal">	
+							<Link to="/about">	
 								View
 							</Link>
 						</div>
@@ -91,32 +73,16 @@ const Home = () => {
 						<h2 className="header_text">What We Are</h2>
 						<br />
 						<p className="subheader_text">
-							Amukpe Community is a communitty in Sapele Delta State. 
-							With love and 
-							unity dvfyd yfewu udhfh gdtf ht id tihin sth reasdon why 
-							you didnet shwi 
-							iup T my place the otehr cay dont get uoddatedn eith the 
-							sstory of this 
-							comunity we lovwe to keep it real and smooth all the dy 
-							of i
-							ut lives and 
-							lives with it as kive hasa offered us with so much 
-							opportunities, dot sir 
-							nad thunj that theh sroyt witg tr xgan an minert frim 
-							nie si that teh oeiolw 
-							iof the kabn will yhfeuy yqyr ncgyewc the goodsa nd 
-							service if tey sdgfy 
-							asjjyf yasid the minerb uu gdewv hkw qace thia sshidte i 
-							wiiudlf nt caome 
-							back agian foir any reason s tat i iwill gaev my 
-							sdyfgyjc'
+							Amukpe Community is a community situated in Sapele Local Government of Delta State. We established this platform to 
+							improve the standard of the community and create awareness to our people. One main aim of this platform
+							is to provide job opportunities to indigenes and non-indigenes of Okpe Kingdom at large and in diaspora. 
 						</p>
 
 						<br />
 						<br />
 
 						<div className="header_button">
-							<Link to="/portal">	
+							<Link to="/about">	
 								View
 							</Link>
 						</div>
@@ -142,17 +108,15 @@ const Home = () => {
 						<h2 className="header_text">Looking for a Job?</h2>
 						<br />
 						<p className="subheader_text">
-							Amukpe Community is a communitty in Sapele Delta State. 
-							With love aand unity dvfyd yfewu udhfh gdtf ht id tihin 
-							sth reasdon why any reason s tat i iwill gaev my sdyfgyjc'. 
-							The community is here to help you.
+							The commmunity is here to help you with job opportunities, you are just one step away. We have so many job openings waiting for you. Don't miss out.
+						 	Register and start applying.
 						</p>
 
 						<br />
 						<br />
 
 						<div className="header_button">
-							<Link to="/apply">	
+							<Link to="/jobopening">	
 								Apply
 							</Link>
 						</div>
